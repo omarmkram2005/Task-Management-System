52043ad10ce06d0bc563bf36987327418c2f0f90
omar mkram <mkramwolf@gmail.com>
2025-09-02 02:37:22 +0300

    final push

--- front-end/src/supabase.js
+++ front-end/src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
